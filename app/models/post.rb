85d97ef31119e842e5d2cae8fc3c1800d5ec69c3
Erin Green <erin.y.green@gmail.com>
2019-02-10 18:42:24 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,18 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction Non-fiction)}
+  validate :is_clickbait?
 
+    CLICKBAIT_PATTERNS = [
+      /Won't Believe/i,
+      /Secret/i,
+      /Top [0-9]*/i,
+      /Guess/i
+    ]
 
-  def validate(post)
-    binding.pry 
-    unless post.title.include? "Won't Believe" || "Secret" || "Top [number]" || "Guess"
-      return false
+    def is_clickbait?
+      if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+        errors.add(:title, "must be clickbait")
+      end
     end
-  end
 end
